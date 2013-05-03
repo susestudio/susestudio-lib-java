d2f0115078230f34df32be2b8b9e6b00bf07cf20
Johannes Renner <jrenner@suse.de>
2013-05-03 11:14:20 +0200

    Remove unused import

--- src/test/java/com/suse/studio/client/test/ResponseTest.java
+++ src/test/java/com/suse/studio/client/test/ResponseTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
